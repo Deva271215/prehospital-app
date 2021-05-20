17888be14b7c0bbbd0789a1b1c8f797400398ff6
Deva271215 <devaputra78@gmail.com>
2021-05-21 02:02:20 +0800

    Save user login data to Shared Preferences

--- app/src/main/java/com/g_one_nursesapp/auth/login/LoginActivity.kt
+++ app/src/main/java/com/g_one_nursesapp/auth/login/LoginActivity.kt
@@ -8,9 +8,11 @@ import com.g_one_nursesapp.MainActivity
 import com.g_one_nursesapp.ProfileActivity
 import com.g_one_nursesapp.R
 import com.g_one_nursesapp.api.RetrofitClient
+import com.g_one_nursesapp.api.response.LoginData
 import com.g_one_nursesapp.api.response.LoginResponse
 import com.g_one_nursesapp.auth.data.storage.SharedPrefManager
 import com.g_one_nursesapp.entity.UserEntity
+import com.g_one_nursesapp.preference.UserPreference
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
 import retrofit2.Call
@@ -18,10 +20,13 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var preference: UserPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        preference = UserPreference(applicationContext)
 
         buttonLogin.setOnClickListener {
             val email = loginEmail.text.toString().trim()
@@ -43,6 +48,9 @@ class LoginActivity : AppCompatActivity() {
                     .enqueue(object: Callback<LoginResponse> {
                         override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                             if (response.isSuccessful) {
+                                val loginData = LoginData(response.body()?.data?.user, response.body()?.data?.access_token)
+                                preference.setLoginData(loginData)
+
                                 val intent = Intent(this@LoginActivity, MainActivity::class.java)
                                 startActivity(intent)
                             } else {
@@ -55,16 +63,6 @@ class LoginActivity : AppCompatActivity() {
                         }
 
                     })
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        if(SharedPrefManager.getInstance(this).isLoggedIn){
-            val intent = Intent(applicationContext, ProfileActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
         }
     }
 }
