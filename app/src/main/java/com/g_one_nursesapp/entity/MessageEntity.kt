c7e1d2404422c148672f79e310c8500e8614e045
Deva271215 <devaputra78@gmail.com>
2021-05-24 00:40:58 +0800

    Added more field

--- app/src/main/java/com/g_one_nursesapp/entity/MessageEntity.kt
+++ app/src/main/java/com/g_one_nursesapp/entity/MessageEntity.kt
@@ -1,8 +1,12 @@
 package com.g_one_nursesapp.entity
 
+import androidx.annotation.Nullable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.sql.Time
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 @Entity(tableName = "messages")
 class MessageEntity(
@@ -12,6 +16,21 @@ class MessageEntity(
     @ColumnInfo
     val message: String,
 
+    @ColumnInfo
+    @Nullable
+    val result: String?,
+
+    @ColumnInfo
+    @Nullable
+    val response: String?,
+
+    @ColumnInfo
+    @Nullable
+    val condition: String?,
+
+    @ColumnInfo(name = "time")
+    val time: String,
+
     @ColumnInfo(name = "is_deleted")
-    val isDeleted: Boolean = false
+    val isDeleted: Boolean = false,
 )
