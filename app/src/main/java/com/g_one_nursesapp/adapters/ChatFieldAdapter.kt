7998f6b115c5be6b16838d92f849dcff4fd0ff85
Deva271215 <devaputra78@gmail.com>
2021-05-26 05:30:35 +0800

    Change item.message.time to item.message.creationTime

--- app/src/main/java/com/g_one_nursesapp/adapters/ChatFieldAdapter.kt
+++ app/src/main/java/com/g_one_nursesapp/adapters/ChatFieldAdapter.kt
@@ -1,6 +1,5 @@
 package com.g_one_nursesapp.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,9 @@ import com.bumptech.glide.Glide
 import com.g_one_nursesapp.R
 import com.g_one_nursesapp.entity.relation.MessageWithAttachments
 import kotlinx.android.synthetic.main.chat_list.view.*
+import java.text.SimpleDateFormat
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class ChatFieldAdapter: RecyclerView.Adapter<ChatFieldAdapter.ViewHolder>() {
     private var messages = emptyList<MessageWithAttachments>()
@@ -22,7 +24,7 @@ class ChatFieldAdapter: RecyclerView.Adapter<ChatFieldAdapter.ViewHolder>() {
         fun bind(item: MessageWithAttachments) {
             with(itemView) {
                 title_chat.text = item.message.message
-                time.text = item.message.time
+                time.text = item.message.creationTime
                 if (!item.message.response.isNullOrEmpty()) {
                     text_respon.visibility = View.VISIBLE
                     value_respon.visibility = View.VISIBLE
