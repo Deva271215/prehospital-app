ca90fcbae2324b687898f7567dc2d6162544b208
Deva271215 <devaputra78@gmail.com>
2021-05-26 05:31:40 +0800

    Added JSON serialization in message entity

--- app/src/main/java/com/g_one_nursesapp/entity/MessageEntity.kt
+++ app/src/main/java/com/g_one_nursesapp/entity/MessageEntity.kt
@@ -4,37 +4,43 @@ import androidx.annotation.Nullable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 import java.sql.Time
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 @Entity(tableName = "messages")
 class MessageEntity(
     @PrimaryKey
+    @field:SerializedName("id")
     val id: String,
 
     @ColumnInfo
+    @field:SerializedName("message")
     val message: String,
 
     @ColumnInfo
     @Nullable
-    val result: String?,
+    @field:SerializedName("result")
+    val result: String? = null,
 
     @ColumnInfo
     @Nullable
-    val response: String?,
+    @field:SerializedName("response")
+    val response: String? = null,
 
     @ColumnInfo
     @Nullable
-    val condition: String?,
+    @field:SerializedName("condition")
+    val condition: String? = null,
 
     @ColumnInfo
     @Nullable
-    val action: String?,
+    @field:SerializedName("action")
+    val action: String? = null,
 
-    @ColumnInfo(name = "time")
-    val time: String,
-
-    @ColumnInfo(name = "is_deleted")
-    val isDeleted: Boolean = false,
+    @ColumnInfo(name = "creation_time")
+    @field:SerializedName("creation_time")
+    val creationTime: String? = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")),
 )
