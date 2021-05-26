e25404d2d83e340ee1f66e87c192c06d4faaa3f1
Deva271215 <devaputra78@gmail.com>
2021-05-27 01:16:01 +0800

    Fix sign up logic

--- app/src/main/java/com/g_one_nursesapp/auth/signup/SignupActivity.kt
+++ app/src/main/java/com/g_one_nursesapp/auth/signup/SignupActivity.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.g_one_nursesapp.R
-import com.g_one_nursesapp.api.DefaultResponse
 import com.g_one_nursesapp.api.RetrofitClient
+import com.g_one_nursesapp.api.response.SignUpResponse
 import com.g_one_nursesapp.auth.login.LoginActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.g_one_nursesapp.entity.UserEntity
 import kotlinx.android.synthetic.main.activity_signup.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -71,14 +71,15 @@ class SignupActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            RetrofitClient.instance.createUser(email, groupName, password, noHp)
-                .enqueue(object: Callback<DefaultResponse>{
+            val user = UserEntity(email = email, groupName = groupName, password = password, noHp = noHp)
+            RetrofitClient.instance.createUser(user)
+                .enqueue(object: Callback<SignUpResponse>{
 
-                    override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
+                    override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
                         Toast.makeText(this@SignupActivity, "Error", Toast.LENGTH_LONG).show()
                     }
 
-                    override fun onResponse(call: Call<DefaultResponse>, response: Response<DefaultResponse>) {
+                    override fun onResponse(call: Call<SignUpResponse>, response: Response<SignUpResponse>) {
 
                       if (response.code() == 201){
                             val intent = Intent(this@SignupActivity, LoginActivity::class.java)
