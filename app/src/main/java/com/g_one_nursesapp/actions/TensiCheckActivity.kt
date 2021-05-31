fb0b8a1853b941da90d5b8e77a945027c4583f18
Deva271215 <devaputra78@gmail.com>
2021-06-01 00:46:48 +0800

    Added logic to TensiCheckActivity.kt

--- app/src/main/java/com/g_one_nursesapp/actions/TensiCheckActivity.kt
+++ app/src/main/java/com/g_one_nursesapp/actions/TensiCheckActivity.kt
@@ -1,28 +1,63 @@
 package com.g_one_nursesapp.actions
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.g_one_nursesapp.R
+import androidx.lifecycle.ViewModelProvider
+import com.g_one_nursesapp.ChatFieldActivity
+import com.g_one_nursesapp.databinding.ActivityTensiCheckBinding
+import com.g_one_nursesapp.entity.MessageEntity
+import com.g_one_nursesapp.viewmodels.TensiCheckViewModel
+import java.util.*
 
 class TensiCheckActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTensiCheckBinding
+    private lateinit var tensiCheckViewModel: TensiCheckViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tensi_check)
+        binding = ActivityTensiCheckBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tensiCheckViewModel = ViewModelProvider(this).get(TensiCheckViewModel::class.java)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Cek Tensi"
 
-//        setButtonSubmit()
+        onSubmitButtonClicked()
     }
-
-//    private fun setButtonSubmit(){
-//        button_submite.setOnClickListener {
-//            Toast.makeText(this@TensiCheckActivity, "Value Submited", Toast.LENGTH_LONG).show()
-//        }
-//    }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun onSubmitButtonClicked() {
+        binding.buttonSubmite.setOnClickListener {
+            val etSystole = binding.inputSystole.text.toString()
+            val etDiastole = binding.inputDiastole.text.toString()
+
+            when {
+                etSystole.isNullOrEmpty() -> {
+                    Toast.makeText(this, "Input systole tidak boleh kosong", Toast.LENGTH_LONG).show()
+                }
+                etDiastole.isNullOrEmpty() -> {
+                    Toast.makeText(this, "Input diastole tidak boleh kosong", Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    // Create message
+                    val message = MessageEntity(
+                        id = UUID.randomUUID().toString(),
+                        message = "Cek Tensi",
+                        result = "Systole: $etSystole, Diastole: $etDiastole",
+                    )
+                    tensiCheckViewModel.insertOneMessage(message)
+
+                    val intent = Intent(this, ChatFieldActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
