a290a8bb60ea416d0397ff7173bd811c73710ca7
Deva271215 <devaputra78@gmail.com>
2021-06-02 04:41:16 +0800

    Added global variable

---
+++ app/src/main/java/com/g_one_nursesapp/entity/SymtompEntity.kt
@@ -0,0 +1,164 @@
+package com.g_one_nursesapp.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class SymtompEntity(
+    @field:SerializedName("nyeri")
+    var nyeri: Int? = 0,
+
+    @field:SerializedName("mual_dan_muntal")
+    var mualDanMuntah: Int? = 0,
+
+    @field:SerializedName("kelelahan")
+    var kelelahan: Int? = 0,
+
+    @field:SerializedName("batuk")
+    var batuk: Int? = 0,
+
+    @field:SerializedName("demam_tinggi")
+    var demamTinggi: Int? = 0,
+
+    @field:SerializedName("kesulitan_bernafas")
+    var kesulitanBernafas: Int? = 0,
+
+    @field:SerializedName("sakit_kepala")
+    var sakitKepala: Int? = 0,
+
+    @field:SerializedName("sakit_leher")
+    var sakitLeher: Int? = 0,
+
+    @field:SerializedName("sakit_lutut")
+    var sakitLutut: Int? = 0,
+
+    @field:SerializedName("nyeri_sendi_pinggul")
+    var nyeriSendiPinggul: Int? = 0,
+
+    @field:SerializedName("kelemahan_otot")
+    var kelemahanOtot: Int? = 0,
+
+    @field:SerializedName("leher_kaku")
+    var leherKaku: Int? = 0,
+
+    @field:SerializedName("bengkak_atau_memar")
+    var bengkakAtauMemar: Int? = 0,
+
+    @field:SerializedName("gerakan_kaku")
+    var gerakanKaku: Int? = 0,
+
+    @field:SerializedName("kelemahan_satu_sisi_tubuh")
+    var kelemahanSatuSisiTubuh: Int? = 0,
+
+    @field:SerializedName("sensor_berubah")
+    var sensorBerubah: Int? = 0,
+
+    @field:SerializedName("riwayat_keluarga")
+    var riwayatKeluarga: Int? = 0,
+
+    @field:SerializedName("dahak_berlendir")
+    var dahakBerlendir: Int? = 0,
+
+    @field:SerializedName("sakit_saat_berjalan")
+    var sakitSaatBerjalan: Int? = 0,
+
+    @field:SerializedName("kebingungan")
+    var kebingungan: Int? = 0,
+
+    @field:SerializedName("hilang_kesadaran")
+    var hilangKesadaran: Int? = 0,
+
+    @field:SerializedName("penurunan_kesadaran")
+    var penurunanKesadaran: Int? = 0,
+
+    @field:SerializedName("pengelihatan_kabur")
+    var pengelihatanKabur: Int? = 0,
+
+    @field:SerializedName("telinga_berdering")
+    var telingaBerdering: Int? = 0,
+
+    @field:SerializedName("mati_rasa_atau_kesemutan")
+    var matiRasaAtauKesemutan: Int? = 0,
+
+    @field:SerializedName("kesulitan_mengontrol_bab_dan_bak")
+    var kesulitanMengontrolBABdanBAK: Int? = 0,
+
+    @field:SerializedName("kesulitan_berjalan")
+    var kesulitanBerjalan: Int? = 0,
+
+    @field:SerializedName("lumpuh")
+    var lumpuh: Int? = 0,
+
+    @field:SerializedName("lupa_kejadian")
+    var lupaKejadian: Int? = 0,
+
+    @field:SerializedName("gelisah")
+    var gelisah: Int? = 0,
+
+    @field:SerializedName("kesulitan_berbicara")
+    var kesulitanBerbicara: Int? = 0,
+
+    @field:SerializedName("nadi_lemah")
+    var nadiLemah: Int? = 0,
+
+    @field:SerializedName("tangan_dan_kaki_dingin")
+    var tanganDanKakiDingin: Int? = 0,
+
+    @field:SerializedName("kehilangan_keseimbangan")
+    var kehilanganKeseimbangan: Int? = 0,
+
+    @field:SerializedName("kejang")
+    var kejang: Int? = 0,
+
+    @field:SerializedName("kejang_otot")
+    var kejangOtot: Int? = 0,
+
+    @field:SerializedName("tulang_mencuat_keluar_dari_kulit")
+    var tulangMencuatKeluarDariKulit: Int? = 0,
+
+    @field:SerializedName("sulit_menggerakkan_bagian_tubuh")
+    var sulitMenggerakkanBagianTubuh: Int? = 0,
+
+    @field:SerializedName("deformitas")
+    var deformitas: Int? = 0,
+
+    @field:SerializedName("kemampuan_gerak_terbatas")
+    var kemampuanGerakTerbatas: Int? = 0,
+
+    @field:SerializedName("detak_jantung_bertambah")
+    var detakJantungBertambah: Int? = 0,
+
+    @field:SerializedName("pucat")
+    var pucat: Int? = 0,
+
+    @field:SerializedName("lemas")
+    var lemas: Int? = 0,
+
+    @field:SerializedName("otot_kaku")
+    var ototKaku: Int? = 0,
+
+    @field:SerializedName("bentuk_sendi_tidak_normal")
+    var bentukSendiTidakNormal: Int? = 0,
+
+    @field:SerializedName("kram_otot")
+    var kramOtot: Int? = 0,
+
+    @field:SerializedName("perut_kaku")
+    var perutKaku: Int? = 0,
+
+    @field:SerializedName("sakit_perut")
+    var sakitPerut: Int? = 0,
+
+    @field:SerializedName("berkeringat")
+    var berkeringat: Int? = 0,
+
+    @field:SerializedName("tekanan_darah_rendah")
+    var tekananDarahRendah: Int? = 0,
+
+    @field:SerializedName("pendarahan_akut")
+    var pendarahanAkut: Int? = 0,
+
+    @field:SerializedName("denyut_nadi_cepat")
+    var denyutNadiCepat: Int? = 0,
+
+    @field:SerializedName("warna_kulit_kebiruan")
+    var warnaKulitKebiruan: Int? = 0,
+)
